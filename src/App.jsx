af8744f2ec4788c4a08ce99533809a309d122623
GootaLK <lokosmen1235@gmail.com>
2022-04-08 00:32:46 -0500

    Feat: Add App-section class to Hero and resolve its styles

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App () {
     <div className='App'>
       <Header visible={false} />
       <main>
-        <Hero />
+        <Hero newClass='App-section' />
       </main>
     </div>
   )
