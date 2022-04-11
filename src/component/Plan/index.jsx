903741cd2ea3f9fec00ebabdfdf4eb5c2a18f715
GootaLK <lokosmen1235@gmail.com>
2022-04-10 22:35:56 -0500

    Feat: Add Plan component

---
+++ src/component/Plan/index.jsx
@@ -0,0 +1,41 @@
+import { useLayoutEffect, useRef } from 'react'
+
+import './Plan.css'
+
+import Button from '../Button'
+
+function Plan ({
+  backgroundUrl,
+  title,
+  currency,
+  description,
+  link,
+  buttonColor,
+  buttonText
+}) {
+  const plan = useRef()
+
+  useLayoutEffect(() => {
+    if (typeof backgroundUrl === 'string') {
+      plan.current.style.setProperty('background-image', `url(${backgroundUrl})`)
+    }
+  }, [])
+
+  return (
+    <div className='Plan' ref={plan}>
+      <h2>{title}</h2>
+      <h3>{currency}</h3>
+      <p>{description}</p>
+      <Button
+        color={buttonColor}
+        href={link}
+        target='_blank'
+        rel='noreferrer nopener'
+      >
+        {buttonText}
+      </Button>
+    </div>
+  )
+}
+
+export default Plan
