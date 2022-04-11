5aeceea347757fd30b40b022f182cd754e34f13f
GootaLK <lokosmen1235@gmail.com>
2022-04-10 19:03:56 -0500

    Feat: Add links to Hero

--- src/component/Hero/index.jsx
+++ src/component/Hero/index.jsx
@@ -17,11 +17,18 @@ function Hero ({ newClass = '' }) {
       <div className='Hero-content'>
         <h2>{language.heroTitle}</h2>
         <img src='https://cnbl-cdn.bamgrid.com/assets/7ecc8bcb60ad77193058d63e321bd21cbac2fc67281dbd9927676ea4a4c83594/original' alt='Disney logo' />
-        <Button color='#6421ff'>{language.offerComboPlusButton}</Button>
+        <Button
+          color='#6421ff'
+          href='https://www.disneyplus.com/sign-up?type=bundle'
+          target='_blank'
+          rel='noreferrer noopener'
+        >
+          {language.offerComboPlusButton}
+        </Button>
         <p>
-          <span><a href=''>{language.signupOnlyMonthly}</a></span>
+          <span><a href='https://www.disneyplus.com/sign-up?type=standard'>{language.signupOnlyMonthly}</a></span>
           {' | '}
-          <span><a href=''>{language.signupOnlyYearly}</a></span>
+          <span><a href='https://www.disneyplus.com/billing?skuInterval=annual&toggle=shown'>{language.signupOnlyYearly}</a></span>
         </p>
       </div>
 
