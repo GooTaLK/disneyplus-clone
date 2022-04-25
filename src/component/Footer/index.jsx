9eb6146e3b8581484f01f67f51d36df9bb09ad1f
GootaLK <lokosmen1235@gmail.com>
2022-04-25 17:53:45 -0500

    Feat: Add target _blank to links on Footer

--- src/component/Footer/index.jsx
+++ src/component/Footer/index.jsx
@@ -31,32 +31,56 @@ function Footer ({ newClass = '' }) {
         </span>
         <ul className='Footer-links fine'>
           <li>
-            <a href='https://disneytermsofuse.com/english/'>
+            <a
+              href='https://disneytermsofuse.com/english/'
+              target='_blank'
+              rel='noreferrer noopener'
+            >
               {language.termsOfUse}
             </a>
           </li>
           <li>
-            <a href='https://www.disneyplus.com/legal/privacy-policy'>
+            <a
+              href='https://www.disneyplus.com/legal/privacy-policy'
+              target='_blank'
+              rel='noreferrer noopener'
+            >
               {language.privacyPolicy}
             </a>
           </li>
           <li>
-            <a href='https://preferences-mgr.trustarc.com/?pid=disney01&aid=disneyplus01&type=disneyplus'>
+            <a
+              href='https://preferences-mgr.trustarc.com/?pid=disney01&aid=disneyplus01&type=disneyplus'
+              target='_blank'
+              rel='noreferrer noopener'
+            >
               {language.interestBasedAds}
             </a>
           </li>
           <li>
-            <a href='https://help.disneyplus.com/csp?id=csp_article_content&article=devices-supported'>
+            <a
+              href='https://help.disneyplus.com/csp?id=csp_article_content&article=devices-supported'
+              target='_blank'
+              rel='noreferrer noopener'
+            >
               {language.supportedDevices}
             </a>
           </li>
           <li>
-            <a href='https://help.disneyplus.com/'>
+            <a
+              href='https://help.disneyplus.com/'
+              target='_blank'
+              rel='noreferrer noopener'
+            >
               {language.helpCenter}
             </a>
           </li>
           <li>
-            <a href='https://nam12.safelinks.protection.outlook.com/?url=https%3A%2F%2Fhelp.disneyplus.com%2Fcsp%3Fid%3Dcsp_article_content_international%26sys_kb_id%3D8d061aaadb7004903c0cf158bf9619ed&data=04%7C01%7Cpamela.yuli%40disneystreaming.com%7Cf18478267d574292b5f608d99b03a314%7C65f03ca86d0a493e9e4ac85ac9526a03%7C1%7C0%7C637711262201165571%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C1000&sdata=mp2uZAhHUXOORZTc9PKW1UJnDIfQ%2BLi7JuOekdVSYxU%3D&reserved=0'>
+            <a
+              href='https://nam12.safelinks.protection.outlook.com/?url=https%3A%2F%2Fhelp.disneyplus.com%2Fcsp%3Fid%3Dcsp_article_content_international%26sys_kb_id%3D8d061aaadb7004903c0cf158bf9619ed&data=04%7C01%7Cpamela.yuli%40disneystreaming.com%7Cf18478267d574292b5f608d99b03a314%7C65f03ca86d0a493e9e4ac85ac9526a03%7C1%7C0%7C637711262201165571%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C1000&sdata=mp2uZAhHUXOORZTc9PKW1UJnDIfQ%2BLi7JuOekdVSYxU%3D&reserved=0'
+              target='_blank'
+              rel='noreferrer noopener'
+            >
               {language.aboutUs}
             </a>
           </li>
